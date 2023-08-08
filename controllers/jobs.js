1fbc4f4b4bf28dd5e8f35de42a037a260193532e
Quan Bui <quanbui021001@gmail.com>
2023-08-08 20:33:39 +0300

    jobs route

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,12 +1,27 @@
+const {StatusCodes} = require('http-status-codes')
+const Job = require('../models/Job')
+const {BadRequestError, NotFoundError} = require('../errors')
+
 const getAllJobs = async(req, res) => {
-    res.send('all jobs')
+    const jobs = await Job.find({createdBy: req.user.userId}).sort('createdAt')
+    res.status(StatusCodes.OK).json({jobs, count: jobs.length})
 }
 
 const getJob = async(req, res) => {
-    res.send('getJob')
+    const {user: {userId}, params: {id: jobId}} = req
+    const job = await Job.findOne({
+        _id: jobId,
+        createdBy: userId
+    })
+    if (!job) {
+        throw new NotFoundError(`No job with id ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json(job)
 }
 const createJob = async(req, res) => {
-    res.json(req.user)
+    req.body.createdBy = req.user.userId
+    const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json(job)
 }
 const updateJob = async(req, res) => {
     res.send('updateJob')
